b536f7535ae20987fc7227e1e46a6cb0b1ebbdf1
polovni <luka.polovic11@gmail.com>
2025-04-13 21:11:43 +0200

    implementing merkleRoot and adding method addTransaction in Block class

--- src/Block.java
+++ src/Block.java
@@ -1,15 +1,16 @@
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Block {
 
     public String hash;
     public String previousHash;
-    private String data;
+    public String merkleRoot;
+    public ArrayList<Transaction> transactions = new ArrayList<>();
     private long timeStamp;
     private int nonce;
 
-    public Block(String data, String previousHash) {
-        this.data = data;
+    public Block(String previousHash) {
         this.previousHash = previousHash;
         this.timeStamp = new Date().getTime();
         this.hash = calculateHash();
@@ -18,15 +19,29 @@ public class Block {
     //Calculate new hash based on blocks contents
     public String calculateHash() {
         return StringUtil.applySha256(
-                previousHash + Long.toString(timeStamp) + Integer.toString(nonce) + data);
+                previousHash + Long.toString(timeStamp) + Integer.toString(nonce) + merkleRoot);
     }
 
     public void mineBlock(int difficulty) {
+        merkleRoot = StringUtil.getMerkleRoot(transactions);
         String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
         while(!hash.substring( 0, difficulty).equals(target)) {
             nonce ++;
             hash = calculateHash();
         }
         System.out.println("Block Mined!!! : " + hash);
+    }
+
+    public boolean addTransaction(Transaction transaction) {
+        if(transaction == null) return false;
+        if(!previousHash.equals("0")) {
+            if(!transaction.processTransaction()) {
+                System.out.println("Transaction failed to process. Discarding transaction.");
+                return false;
+            }
+        }
+        transactions.add(transaction);
+        System.out.println("Transaction Successfully added to Block");
+        return true;
     }
 }
